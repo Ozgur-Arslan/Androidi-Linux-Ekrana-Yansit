3dea5f8480887effd8c5c6edd352507e8acd8fbe
Ozgur-Arslan <konarslan@hotmail.com>
2025-08-22 15:56:55 +0300

    Update desktop_ikon_ekle.sh
    Signed-off-by: Ozgur-Arslan <konarslan@hotmail.com>

--- desktop_ikon_ekle.sh
+++ desktop_ikon_ekle.sh
@@ -17,13 +17,20 @@ cp tablete_baglan.desktop $HOME/Masaüstü/
 chmod +x $HOME/Masaüstü/tablete_baglan.desktop
 
 #Program klasörünü SİL
-find ~ -type d -exec sh -c '
-  for dir do
+#find ~ -type d -exec sh -c '
+ # for dir do
+  #  if [ -f "$dir/kur.sh" ] && [ -f "$dir/desktop_ikon_ekle.sh" ] && [ -f "$dir/tablete_baglan.desktop" ] && [ -f "$dir/tablete_baglan.sh" ]; then
+   #   echo "🗑️ Siliniyor: $dir"
+    #  rm -rf "$dir"
+    #fi
+  #done
+#' sh {} +
+
+find "$HOME" -type f \( -name "kur.sh" -o -name "desktop_ikon_ekle.sh" -o -name "tablete_baglan.desktop" -o -name "tablete_baglan.sh" \) | while read file; do
+    dir=$(dirname "$file")
     if [ -f "$dir/kur.sh" ] && [ -f "$dir/desktop_ikon_ekle.sh" ] && [ -f "$dir/tablete_baglan.desktop" ] && [ -f "$dir/tablete_baglan.sh" ]; then
-      echo "🗑️ Siliniyor: $dir"
-      rm -rf "$dir"
+        rm -rf "$dir"
     fi
-  done
-' sh {} +
+done
 
 
